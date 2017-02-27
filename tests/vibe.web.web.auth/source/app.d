7e57acfda142cead081a781b92f612e0a368efb8
Sebastian Wilzbach <seb@wilzba.ch>
2017-02-27 14:10:33 +0100

    AuthInfo

--- tests/vibe.web.web.auth/source/app.d
+++ tests/vibe.web.web.auth/source/app.d
@@ -14,20 +14,22 @@ import std.datetime;
 import std.format : format;
 
 
-shared static this()
+int main()
 {
-	auto settings = new HTTPServerSettings;
-	settings.port = 9128;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-    auto router = new URLRouter;
-    router.registerWebInterface(new Service);
-    listenHTTP(settings, router);
-
     runTask({
         scope (exit) exitEventLoop();
 
+		auto settings = new HTTPServerSettings;
+		settings.port = 9128;
+		settings.bindAddresses = ["::1", "127.0.0.1"];
+    	auto router = new URLRouter;
+    	router.registerWebInterface(new Service);
+    	listenHTTP(settings, router);
+
+        logInfo("RUN");
         void test(string url, string user, HTTPStatus expected)
         nothrow {
+			logInfo("Test: %s", url);
             try {
                 requestHTTP("http://127.0.0.1:9128"~url, (scope req) {
                     if (user !is null) req.addBasicAuth(user, "secret");
@@ -40,27 +42,34 @@ shared static this()
             }
         }
 
-        test("/public", null, HTTPStatus.ok);
-        test("/any", null, HTTPStatus.unauthorized);
-        test("/any", "stacy", HTTPStatus.ok);
-        test("/any_a", null, HTTPStatus.unauthorized);
-        test("/any_a", "stacy", HTTPStatus.ok);
-        test("/admin", null, HTTPStatus.unauthorized);
-        test("/admin", "admin", HTTPStatus.ok);
-        test("/admin", "peter", HTTPStatus.forbidden);
-        test("/admin", "stacy", HTTPStatus.forbidden);
-        test("/admin_a", null, HTTPStatus.unauthorized);
-        test("/admin_a", "admin", HTTPStatus.ok);
-        test("/admin_a", "peter", HTTPStatus.forbidden);
-        test("/admin_a", "stacy", HTTPStatus.forbidden);
-        test("/member", "admin", HTTPStatus.forbidden);
-        test("/member", "peter", HTTPStatus.ok);
-        test("/member", "stacy", HTTPStatus.forbidden);
-        test("/admin_member", "peter", HTTPStatus.ok);
-        test("/admin_member", "admin", HTTPStatus.ok);
-        test("/admin_member", "stacy", HTTPStatus.forbidden);
+        test("/1/public", null, HTTPStatus.ok);
+        test("/1/any", null, HTTPStatus.unauthorized);
+        test("/1/any", "stacy", HTTPStatus.ok);
+        test("/1/any_a", null, HTTPStatus.unauthorized);
+        test("/1/any_a", "stacy", HTTPStatus.ok);
+        test("/1/admin", null, HTTPStatus.unauthorized);
+        test("/1/admin", "admin", HTTPStatus.ok);
+        test("/1/admin", "peter", HTTPStatus.forbidden);
+        test("/1/admin", "stacy", HTTPStatus.forbidden);
+        test("/1/admin_a", null, HTTPStatus.unauthorized);
+        test("/1/admin_a", "admin", HTTPStatus.ok);
+        test("/1/admin_a", "peter", HTTPStatus.forbidden);
+        test("/1/admin_a", "stacy", HTTPStatus.forbidden);
+        test("/1/member", "admin", HTTPStatus.forbidden);
+        test("/1/member", "peter", HTTPStatus.ok);
+        test("/1/member", "stacy", HTTPStatus.forbidden);
+        test("/1/admin_member", "peter", HTTPStatus.ok);
+        test("/1/admin_member", "admin", HTTPStatus.ok);
+        test("/1/admin_member", "stacy", HTTPStatus.forbidden);
+        test("/2/public", null, HTTPStatus.ok);
+        test("/2/any", "stacy", HTTPStatus.ok);
+        test("/2/admin", "admin", HTTPStatus.ok);
+        test("/3/public", null, HTTPStatus.ok);
+        test("/3/any", "stacy", HTTPStatus.ok);
+        test("/3/admin", "admin", HTTPStatus.ok);
         logInfo("All auth tests successful.");
     });
+    return runEventLoop();
 }
 
 struct Auth {
@@ -71,6 +80,7 @@ struct Auth {
 }
 
 @requiresAuth
+@path("/s1")
 class Service {
     @noAuth void getPublic(HTTPServerResponse res) { res.writeBody("success"); }
     @anyAuth void getAny(HTTPServerResponse res) { res.writeBody("success"); }
@@ -82,8 +92,45 @@ class Service {
 
     @noRoute Auth authenticate(HTTPServerRequest req, HTTPServerResponse res)
     {
+    	import std.stdio;
+    	writeln("AAAA");
         Auth ret;
         ret.username = performBasicAuth(req, res, "test", (user, pw) { return pw == "secret"; });
         return ret;
     }
+}
+
+@requiresAuth
+class ServiceWithAuth {
+    @noRoute Auth authenticate(HTTPServerRequest req, HTTPServerResponse res)
+    {
+        Auth ret;
+        ret.username = performBasicAuth(req, res, "test", (user, pw) { return pw == "secret"; });
+        return ret;
+    }
+}
+
+@path("/s2")
+class InheritedService : ServiceWithAuth{
+    @noAuth void getPublic(HTTPServerResponse res) { res.writeBody("success"); }
+    @anyAuth void getAny(HTTPServerResponse res) { res.writeBody("success"); }
+    @auth(Role.admin) void getAdmin(HTTPServerResponse res) { res.writeBody("success"); }
+}
+
+
+Auth myAuth(HTTPServerRequest req, HTTPServerResponse res)
+{
+    Auth ret;
+    ret.username = performBasicAuth(req, res, "test", (user, pw) { return pw == "secret"; });
+    import std.stdio;
+    writeln("FOO");
+    return ret;
+}
+
+@requiresAuth!myAuth
+@path("/s3")
+class ServiceWithUDA {
+    @noAuth void getPublic(HTTPServerResponse res) { res.writeBody("success"); }
+    @anyAuth void getAny(HTTPServerResponse res) { res.writeBody("success"); }
+    @auth(Role.admin) void getAdmin(HTTPServerResponse res) { res.writeBody("success"); }
 }
